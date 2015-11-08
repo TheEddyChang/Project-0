b106ae57008b9267acfd4b482be88099038d2eef
TheEddyChang <ehchang58@yahoo.com>
2015-11-08 15:42:31 -0800

    minor changes, avatars not working yet

--- main.js
+++ main.js
@@ -2,6 +2,8 @@ $(document).ready(function() {
 
 	console.log("JS is loaded!");
 
+    var counter1 = 0;
+    var counter2 = 0;
 
 
 	$('body').keydown(function(key) { //using keys m(keycode: 77) for player 1 to move and z(keycode: 90) for player 2
@@ -11,54 +13,84 @@ $(document).ready(function() {
 			case 77:
 
 				$('#player1').animate({
-					left: "+=10px"
+					left: "+=25px"
 				}, 'fast');
+				counter1 +=1;
 				break;
 			case 90:
 				$('#player2').animate({
-					left: "+=10px"
-				}, 'fast')
-	;			break;
+					left: "+=25px"
+				}, 'fast');
+				counter2 +=1;
+				break;
 			default:
 				break;
 		}
 	});
+
+	function winner () {
+		if (counter1 === 37) {
+			$("#racetrack").html("PLAYER ONE WINS!!!");
+			if(counter2 === 37) {
+				$("#racetrack").html("PLAYER TWO WINS!!!");
+			}
+			   if(counter1 === 37 && counter2 === 37);
+			   $("#racetrack").html("TIE GAME!!!!");
+		}
+	}
+
+
+
 
 
 
 	//var playerCar = function() {  //function for player choosing car...cannot choose more than once..
 
 
-	//player1 can choose which car
-	$('#btn').click(function() {
-		$('#player1').append('<img src="http://icones.gratuites.web.free.fr/data/Classic%20Car%20Icons/Classic%20Car.ico">');
+	//player1 can choose which AVATARS
+	
 
-	});
+	    $("#btn1").on("click", function() {
+		$("#player1").append('<img src="http://icones.gratuites.web.free.fr/data/Classic%20Car%20Icons/Classic%20Car.ico">');
+        
+        });
+        $("#btn2").on("click", function() {
+        $("#player1").append('<img src="http://icones.gratuites.web.free.fr/data/Classic%20Car%20Icons/Minicar.ico">');
+       
+        });
 
-	$('#btn2').click(function() {
-		$('#player1').append('<img src="http://icones.gratuites.web.free.fr/data/Classic%20Car%20Icons/Minicar.ico">');
-
-	});
-
-	$('#btn3').click(function() {
-		$('#player1').append('<img src="http://3.bp.blogspot.com/-wQ976e-5-RM/Ugg_rPAT1mI/AAAAAAAAPm0/NENeQDI9cA0/s1600/usain-bolt.png" height= 220 width= 150>');
-	});
-
-	//still need to write code for player 2... this is very unDRY!!
-
-
-	//div collision to determine winner?
-
-	var raceWinner;
+        $('#btn3').bind("click", function() {
+        	$('#player1').append('<img src="http://3.bp.blogspot.com/-wQ976e-5-RM/Ugg_rPAT1mI/AAAAAAAAPm0/NENeQDI9cA0/s1600/usa$(in-bolt.png" height= 220 width= 150>');
+        });
 
 
-	function winner() {
-		raceWinner = $('#player1').collision('#finish');
-		if(raceWinner.length !==0) {
-			$('#racetrack').text("WINNER!!!!!!!!!");
 
-		}
-	}
+
+
+
+        
+  
+    
+
+
+
+
+
+
+	
+
+
+
+
+
+
+
+
+	
+
+	
+
+
 	
 
 
