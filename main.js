6c5a358ac1140c5b0d478e264a76bb65d6568053
TheEddyChang <ehchang58@yahoo.com>
2015-11-07 18:22:02 -0800

    still working on moving cars

--- main.js
+++ main.js
@@ -2,7 +2,19 @@ $(document).ready(function(){
 
 	console.log("JS is loaded!");
 
-	
+
+
+	$(window).keypress(function(event){
+        if(event.which == "77")
+            $(".racetrack").animate({"left" : "-=30px"});
+
+        if(event.which == "90")
+            $(".racetrack").animate({"left" : "+=30px"});
+		
+
+});
+
+
 
 
 
