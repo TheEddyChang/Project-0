7507a906acf2f6ae354a1e8d794e1f2f234da1ea
TheEddyChang <ehchang58@yahoo.com>
2015-11-07 20:34:38 -0800

    adding player options for cars..not complete

--- main.js
+++ main.js
@@ -4,11 +4,12 @@ $(document).ready(function(){
 
 
 
-	$('body').keydown(function(key){
+	$('body').keydown(function(key){   //using keys m(keycode: 77) for player 1 to move and z(keycode: 90) for player 2
 
 	switch (parseInt(key.which)) {
            
            case 77:
+
                $('#player1').animate({
                    left: "+=10px"
                }, 'fast');
@@ -22,6 +23,27 @@ $(document).ready(function(){
                break;
        }
    });
+
+    var playerCar = function() {  //function for player choosing car...cannot choose more than once..
+
+
+    //player can choose which car
+	$('#btn').click(function() {
+		$('#player1').append('<img src="http://icones.gratuites.web.free.fr/data/Classic%20Car%20Icons/Classic%20Car.ico">');
+
+	});
+
+	$('#btn2').click(function() {
+		$('#player1').append('<img src="http://icones.gratuites.web.free.fr/data/Classic%20Car%20Icons/Minicar.ico">');
+
+	});
+
+	
+
+
+
+   
+
   
 
 
