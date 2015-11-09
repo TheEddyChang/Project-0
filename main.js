c02ff88aad602631f776ca95a34826175de38072
TheEddyChang <ehchang58@yahoo.com>
2015-11-08 17:10:58 -0800

    declaring winner working

--- main.js
+++ main.js
@@ -2,11 +2,13 @@ $(document).ready(function() {
 
 	console.log("JS is loaded!");
 
-    var counter1 = 0;
-    var counter2 = 0;
+    
+        var counter1 = 0; //setting counter to determine a winner. will increase as keys
+        var counter2 = 0;
 
+	$(window).keydown(function(key) { //using keys m(keycode: 77) for player 1 to move and z(keycode: 90) for player 2
 
-	$('body').keydown(function(key) { //using keys m(keycode: 77) for player 1 to move and z(keycode: 90) for player 2
+		
 
 		switch (parseInt(key.which)) {
 
@@ -15,29 +17,33 @@ $(document).ready(function() {
 				$('#player1').animate({
 					left: "+=25px"
 				}, 'fast');
-				counter1 +=1;
+				counter1 ++;
+				console.log(counter1);
 				break;
 			case 90:
 				$('#player2').animate({
 					left: "+=25px"
 				}, 'fast');
-				counter2 +=1;
+				counter2 ++;
+				console.log(counter2);
 				break;
 			default:
 				break;
 		}
-	});
 
-	function winner () {
-		if (counter1 === 37) {
-			$("#racetrack").html("PLAYER ONE WINS!!!");
-			if(counter2 === 37) {
-				$("#racetrack").html("PLAYER TWO WINS!!!");
-			}
-			   if(counter1 === 37 && counter2 === 37);
-			   $("#racetrack").html("TIE GAME!!!!");
+
+	     if (counter1 === 37) {        //IF STATEMENT TO DECLARE A WINNER..need more work on stopping the game when winner is reached
+			$("#racetrack").append("PLAYER ONE WINS!!!");
+			console.log("winner");
 		}
-	}
+			else if(counter2 === 37) {
+				$("#racetrack").append("PLAYER TWO WINS!!!");
+				console.log("winner");
+			}
+			   
+});
+
+
 
 
 
@@ -49,7 +55,7 @@ $(document).ready(function() {
 
 	//player1 can choose which AVATARS
 	
-
+/*
 	    $("#btn1").on("click", function() {
 		$("#player1").append('<img src="http://icones.gratuites.web.free.fr/data/Classic%20Car%20Icons/Classic%20Car.ico">');
         
@@ -63,7 +69,7 @@ $(document).ready(function() {
         	$('#player1').append('<img src="http://3.bp.blogspot.com/-wQ976e-5-RM/Ugg_rPAT1mI/AAAAAAAAPm0/NENeQDI9cA0/s1600/usa$(in-bolt.png" height= 220 width= 150>');
         });
 
-
+*/
 
 
 
@@ -71,27 +77,6 @@ $(document).ready(function() {
         
   
     
-
-
-
-
-
-
-	
-
-
-
-
-
-
-
-
-	
-
-	
-
-
-	
 
 
 
